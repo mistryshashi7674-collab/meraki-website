97598fcd6b57572be2e37804e2dd8244f50b6896
Shashi Mistry <mistryshashi7674@gmail.com>
2026-06-30 22:58:47 +0530

    updated mandala-art page

--- app/mandala-art/page.tsx
+++ app/mandala-art/page.tsx
@@ -54,10 +54,8 @@ export const metadata: Metadata = {
 
 export default function MandalaArtPage() {
   return (
-    <main className="min-h-screen bg-[#FFF9F5] p-10">
-      <h1 className="text-4xl font-bold">Mandala Art</h1>
-
-      <p className="mt-4">This page is under construction.</p>
+    <main className="min-h-screen bg-[#FFF9F5]">
+      <h1 className="text-5xl font-bold text-center pt-20">Handmade Mandala Art</h1>
     </main>
   );
 }
