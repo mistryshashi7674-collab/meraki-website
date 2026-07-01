517887aef7968b34b2044fdbf50d9d19c4bba7d8
Shashi Mistry <mistryshashi7674@gmail.com>
2026-07-01 16:28:15 +0530

    updated mandala art hero page

--- lib/mandalaData.ts
+++ lib/mandalaData.ts
@@ -12,12 +12,12 @@ export const mandalaArt = [
     available: true,
 
     images: [
+      "/images/mandala/yellow-mandala/yellow-asthetic.jpg",
       "/images/mandala/yellow-mandala/yellow-mandala-original.jpg",
       "/images/mandala/yellow-mandala/yellow-mandala.jpg",
       "/images/mandala/yellow-mandala/yellow-hallway.jpg",
       "/images/mandala/yellow-mandala/yellow-studyroom.jpg",
       "/images/mandala/yellow-mandala/yellow-dinnertable.jpg",
-      "/images/mandala/yellow-mandala/yellow-asthetic.jpg",
       "/images/mandala/yellow-mandala/yellow-white-background.jpg",
     ],
   },
@@ -35,12 +35,12 @@ export const mandalaArt = [
     available: true,
 
     images: [
+      "/images/mandala/black-mandala/black-hallroom.jpg",
       "/images/mandala/black-mandala/black-mandala-original.jpg",
       "/images/mandala/black-mandala/black-livingroom.jpg",
       "/images/mandala/black-mandala/black-bedroom.jpg",
       "/images/mandala/black-mandala/black-poojaroom.jpg",
       "/images/mandala/black-mandala/black-closeup.jpg",
-      "/images/mandala/black-mandala/black-hallroom.jpg",
       "/images/mandala/black-mandala/black-white-background.jpg",
     ],
   },
@@ -58,12 +58,12 @@ export const mandalaArt = [
     available: true,
 
     images: [
+      "/images/mandala/black-pink-mandala/black-pink-sofa.jpg",
       "/images/mandala/black-pink-mandala/black-pink-original.jpg",
       "/images/mandala/black-pink-mandala/black-pink-bedroom.jpg",
       "/images/mandala/black-pink-mandala/black-pink-poojaroom.jpg",
       "/images/mandala/black-pink-mandala/black-pink-closeup.jpg",
       "/images/mandala/black-pink-mandala/black-pink-asthetic.jpg",
-      "/images/mandala/black-pink-mandala/black-pink-sofa.jpg",
       "/images/mandala/black-pink-mandala/black-pink-white-background.jpg",
     ],
   },
@@ -81,8 +81,8 @@ export const mandalaArt = [
     available: true,
 
     images: [
-      "/images/mandala/ornate-mandala/ornate-original.jpg",
       "/images/mandala/ornate-mandala/ornate-livingroom.jpg",
+      "/images/mandala/ornate-mandala/ornate-original.jpg",
       "/images/mandala/ornate-mandala/ornate-bedroom.jpg",
       "/images/mandala/ornate-mandala/ornate-poojaroom.jpg",
       "/images/mandala/ornate-mandala/ornate-closeup.jpg",
@@ -104,12 +104,12 @@ export const mandalaArt = [
     available: true,
 
     images: [
+      "/images/mandala/swastik-mandala/swastik-asthetic.jpg",
       "/images/mandala/swastik-mandala/swastik-original.jpg",
       "/images/mandala/swastik-mandala/swastik-livingroom.jpg",
       "/images/mandala/swastik-mandala/swastik-bedroom.jpg",
       "/images/mandala/swastik-mandala/swastik-poojaroom.jpg",
-      "/images/mandala/swastik-mandala/swastik-closeup.jpg",
-      "/images/mandala/swastik-mandala/swastik-asthetic.jpg",
+      "/images/mandala/swastik-mandala/swastik-closeup.jpg",     
       "/images/mandala/swastik-mandala/swastik-white-background.jpg",
     ],
   },
